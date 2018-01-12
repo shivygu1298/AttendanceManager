0ae12e80c8f09bb56101f67f8bc401d7ae6609f1
shivygu1298 <34605915+shivygu1298@users.noreply.github.com>
2018-01-12 22:47:48 +0530

    listViewUpdate

---
+++ app/src/main/java/com/example/shivamgupta/attendancemanager/OnListItemCLick.java
@@ -0,0 +1,13 @@
+package com.example.shivamgupta.attendancemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnListItemCLick extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_list_item_click);
+    }
+}
